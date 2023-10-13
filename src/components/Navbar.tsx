f1b979a252410558c30ecf410bbe7c4ce813859d
andrenavas <andreynanavas@gmail.com>
2023-10-13 14:10:30 -0500

    UPDATE: styles in navbar and sidebar on about view desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar: React.FC = () => {
 
         </div>
       )}
-      <div className='container-title-nav'>
+      <div className='container-logo-nav'>
         <img className='nav-img-logo' src='https://firebasestorage.googleapis.com/v0/b/portfolio-fa111.appspot.com/o/images%2Flogo-andre-navas.png?alt=media&token=3c70f877-581e-44e9-ac45-d07b2a0c5ebf&_gl=1*48flyh*_ga*MjE3MzcyMTAzLjE2ODAzNTU2MDA.*_ga_CW55HF8NVT*MTY5NzIyMTEzMC43MC4xLjE2OTcyMjE5NzMuMzcuMC4w' alt='logo andre navas' />
         {/* <span className='title-nav'>andre-navas</span> */}
       </div>
